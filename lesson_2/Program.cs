fb7f50766451506403769961e4110b6402fba94a
Devtect <kaihanucoz@gmail.com>
2023-10-18 08:30:53 +0200

    Lesson 2/3

---
+++ lesson_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// DATA TYPES IN C#
+
+Console.WriteLine('a'); // char = character
+Console.WriteLine("Hello world"); // string
+Console.WriteLine(123); // int = integer
+Console.WriteLine(1.27f); // float
+Console.WriteLine(6.1239); // double
+Console.WriteLine(68.75624523m); //decimal
+Console.WriteLine(true);  // bool = boolean
+Console.WriteLine(false); // bool = boolean
+
+int myNumber;
+myNumber = 5;
+Console.WriteLine(myNumber);
+
